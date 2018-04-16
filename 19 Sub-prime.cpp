da3e59b5e6580554a2bcb1c3ca4eb094e79af10c
jeffersonjpr <jefferson_michael407@hotmail.com>
2018-04-15 21:06:52 -0300

    15/04/2018

--- 19 Sub-prime.cpp
+++ 19 Sub-prime.cpp
@@ -2,26 +2,19 @@
 using namespace std;
 
 int main(){
-	int nb,nd,x,y,z,a = 1;
+	int nb,nd,x,y,z,b[21],regra = 1;
 	while(true){
-		a = 1;
+		regra = 1;
 		cin >> nb >> nd;
-		if(nb == 0 and nd == 0){
-			break;
-		}
-		int b[nb + 1];
-		cin >> b[1] >> b[2] >> b[3];
+		if(nb == 0 and nd == 0) break;
+		for(int ct = 1;ct <= nb;ct++) cin >> b[ct];
 		for(;nd > 0;nd--){
-			 cin >> x >> y >> z;
-			 if(b[x] < z){
-				 printf("N\n");
-				 a = 0;
-				 break;
-			 }
-
-			 b[x] -= z;
-			 b[y] += z;
+			cin >> x >> y >> z;
+			b[x] -= z;
+			b[y] += z;
 		}
-		if(a)printf("S\n");
+		for(int ct = 1;ct <= nb;ct++) if(b[ct] < 0) regra = 0;
+		if(regra) printf("S\n");
+		else printf("N\n");
 	}
 }

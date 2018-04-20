f7a4ab6cff933026f9c3752dbc8d839c40c37c47
jeffersonjpr <jeffersonjpr>
2018-04-20 11:48:04 +0000

    campo minado correto

--- 23 Campo Minado.cpp
+++ 23 Campo Minado.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
+
 int main(){
 	int camp;
 	cin >> camp;
 	bool regra[camp];
 	int campf[camp] = {0};
 	for (int ct = 0;ct < camp;ct++) cin >> regra[ct];
-	if(regra[0]) regra[1] += 1;
-	if(regra[camp-1]) regra[camp-2] += 1;
+	if(regra[0]){
+		campf[1] += 1;
+		campf[0] += 1;
+	}
+	if(regra[camp - 1]){
+		campf[camp - 2] += 1;
+		campf[camp - 1] += 1;
+	}
 	for (int ct = 1;ct < camp-1;ct++){
 		if(regra[ct]){
-			regra[ct-1] += 1;
-			regra[ct+1] += 1;
+			campf[ct-1] += 1;
+			campf[ct] += 1;
+			campf[ct+1] += 1;
 		}
 	}
 	for (int ct = 0;ct < camp;ct++) printf("%i\n",campf[ct]);

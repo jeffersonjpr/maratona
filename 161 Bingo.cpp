7f446ec4ce012234e0a55d9090d87253d936da5f
Jefferson Michael <jefferson_michael407@hotmail.com>
2018-04-27 15:10:24 -0300

    27/04/18

--- 161 Bingo.cpp
+++ 161 Bingo.cpp
@@ -1,30 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-	bool rule;
-	int n,b,valor,lista[91];
-
+	int n = 1,b,lista[91];
+	bool chave = true,key[91] = {false};
 	while(true){
-		rule = 1;
+	    chave = true;
+	    for(int ct = 0;ct <= 90;ct++) {key[ct] = false;}
+
 		cin >> n >> b;
 		if(n == 0 && b == 0) break;
-		for(int ct =0;ct < b;ct++) cin >> lista[ct];
-		for(int ct1 =1;ct1 < n+1;ct1++){
-			for(int ct2=0;ct2 < b;ct2++){
-				if(rule == 0) break;
-				for(int ct3=0;ct3 <b;ct3++){
-					valor = abs(lista[ct2] - lista[ct3]);
-					if(valor == ct1){
-						rule = 0;
+
+		for(int ct = 0;ct < b;ct++) cin >> lista[ct];
+
+		for(int ct = 0;ct <= n;ct++){
+			for(int ct1 = 0;ct1 < b;ct1++){
+				if(key[ct] == true) break;
+				for(int ct2 = ct1;ct2 < b;ct2++){
+					if(abs(lista[ct1] - lista[ct2]) == ct){
+						key[ct] = true;
 						break;
 					}
 				}
 			}
-			if(rule == true) break;
 		}
-		if(rule) printf("N\n");
-		else printf("Y\n");
+		for(int ct = 0;ct <= n;ct++){
+			if(!key[ct]){
+				chave = false;
+				break;
+			}
+		}
+
+		if(chave) printf("Y\n");
+		else printf("N\n");
 
 	}
 }

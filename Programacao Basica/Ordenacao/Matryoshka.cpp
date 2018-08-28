c3dc13bf6124ad970228e3d80cc6073b43f3b552
Jefferson Michael <jefferson_michael407@hotmail.com>
2018-08-27 22:28:31 -0300

    holy guacamole

--- Programacao Basica/Ordenacao/Matryoshka.cpp
+++ Programacao Basica/Ordenacao/Matryoshka.cpp
@@ -1,35 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int quant = 0,meta[10];
-bool sortdiferenciado(int a,int b){
-	if(a < b){
-		meta[quant] = b;
-		quant++;
-		return a < b;
-	}
-	else return a < b;
-}
+
 int main(){
-	int n,vetor[10];
-	scanf("%i",&n);
+	int n,quant = 0,meta[100004],checa[100004],result[100004];
+	scanf(" %i",&n);
 	for(int i = 0;i<n;i++){
-		scanf("%i",&vetor[i]);
+		scanf(" %i",meta + i);
+		checa[i] = meta[i];
 	}
-	for(int i = 0;i<n;i++){
-		printf("%i ",vetor[i]);
+	sort(checa, checa + n);
+
+	for (int i = 0; i < n; i++){
+		if(meta[i] != checa[i]){
+			result[quant] = meta[i];
+			quant++;
+		}
 	}
-	printf("\n");
-	sort(vetor,vetor + n,sortdiferenciado);
-	for(int i = 0;i<n;i++){
-		printf("%i ",vetor[i]);
-	}
-	printf("\n%i\n",quant);
 	if(quant){
-		sort(meta,meta + quant);
-		for (int i = 0; i < quant; i++){
-			printf("%i ",meta[i]);
+		printf("%i\n",quant);
+		sort(result,result + quant);
+		for(int i = 0; i< quant; i++){
+			printf("%i ",result[i]);
 		}
 		printf("\n");
 	}
+	else printf("%i\n\n",quant);
+
 
 }

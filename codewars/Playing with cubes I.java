f94e0dbd9567e9ba0d65a8f6c3494e00aff9f828
Jefferson Michael <jefferson_michael407@hotmail.com>
2019-10-16 11:18:17 -0300

    Create Playing with cubes I.java
    Simples classe com get e set.

---
+++ codewars/Playing with cubes I.java
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/playing-with-cubes-i/java
+
+//criando classe cubo
+public class Cube{
+  private int side;
+    
+    //função get
+    public int getSide(){
+        return side;
+    }
+    
+    //funçao set
+    public void setSide(int side){
+        this.side = side;
+    }
+}

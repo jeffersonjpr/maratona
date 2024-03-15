be6e98d2b0c17b3beba3411a6e0f0a40e7e203c6
jeffersonjpr <jefferson_michael407@hotmail.com>
2024-03-14 23:25:54 -0300

    best

--- LeetCode/13. Roman to Integer.py
+++ LeetCode/13. Roman to Integer.py
@@ -4,13 +4,13 @@ import unittest
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
         result = 0
-        last = 1
-        for c in s[::-1]:
-            current = roman_dict[c]
-            result += current if current >= last else -current
-            last = current
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i+1]]:
+                result -= roman_dict[s[i]]
+            else:
+                result += roman_dict[s[i]]
         return result
 
 class TestRomanToInt(unittest.TestCase):
